05e2c8db61cde94c18665301292cbcf15ef15a6c
Sam Schwartz <sam.schwartz96@gmail.com>
2020-04-06 00:08:29 -0500

    finish login.gohtml

--- internal/handler/oauth.go
+++ internal/handler/oauth.go
@@ -8,24 +8,40 @@ import (
 
 // OauthHandler handles authorization and authentication to oauth clients
 type OauthHandler struct {
-	authTemplate *template.Template
+	loginTemplate *template.Template
+	authTemplate  *template.Template
 }
 
 // CreateOauthHandler just intantiates an OauthHandler
 func CreateOauthHandler() (*OauthHandler, error) {
-	authTemplate, err := template.ParseFiles("templates/oauth.gohtml")
+	loginT, err := template.ParseFiles("templates/oauth/login.gohtml")
+	authT, err := template.ParseFiles("templates/oauth/auth.gohtml")
 	if err != nil {
 		return nil, err
 	}
 
 	return &OauthHandler{
-		authTemplate: authTemplate,
+		loginTemplate: loginT,
+		authTemplate:  authT,
 	}, nil
 }
 
 func (h *OauthHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	err := h.authTemplate.Execute(res, nil)
-	if err != nil {
-		fmt.Println("error executing auth template: ", err)
+	if req.Method == http.MethodGet {
+		var head string
+		var err error
+
+		head, req.URL.Path = ShiftPath(req.URL.Path)
+
+		switch head {
+		case "login":
+			err = h.loginTemplate.Execute(res, nil)
+		case "authorize":
+			err = h.authTemplate.Execute(res, nil)
+		}
+
+		if err != nil {
+			fmt.Println("error executing template: ", err)
+		}
 	}
 }
